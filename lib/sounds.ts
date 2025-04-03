30a972e39cd241bdb6f1e695f5cc5c52c143daeb
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2025-04-03 10:17:35 +0100

    add boo sound on item missed

--- lib/sounds.ts
+++ lib/sounds.ts
@@ -1,3 +1,3 @@
 export const correctSound = new Audio('/sounds/correct-2.mp3');
 export const wrongSound = new Audio('/sounds/wrong-1.mp3');
-export const missedSound = new Audio('/sounds/wrong-1.mp3');
+export const missedSound = new Audio('/sounds/fast-boo.mp3');
